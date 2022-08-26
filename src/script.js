f216770491c74d86dc20294bd1dd7fa38f1281ea
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2022-08-26 17:52:29 -0300

    Comandos da cobra + comida

--- src/script.js
+++ src/script.js
@@ -3,34 +3,60 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x:8 * box,
-    y:8* box
+    x: 8 * box,
+    y: 8 * box
 }
 
-let direction = "right"
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+};
 
-function createBG(){
+function createBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16*box,16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createSnake(){
-    for(i=0;i< snake.length;i++){
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = 'green';
-        context.fillRect(snake[i].x,snake[i].y,box,box);
-
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function initGame(){
-    createBG()
-    createSnake()
-    let snakeX = snake[0].X;
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
+}
+
+document.addEventListener("keydown",update)
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";   
+}
+
+function initGame() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+
+
+    createBG();
+    createSnake();
+    drawFood();
+
+    let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-    if(direction == 'right') snakeX += box;
-    if(direction == 'left') snakeX -= box;
-    if(direcion == 'up') snakeY -= box;
-    if(direcion == 'down') snakeY += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -43,4 +69,4 @@ function initGame(){
 
 }
 
-let game = setInterval(initGame,100)
+let game = setInterval(initGame, 100)
